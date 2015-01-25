6817744a194850dd849106616f40eec0c769dc8f
leonardpuhl <puhl@campus.tu-berlin.de>
2015-01-25 18:36:04 +0100

    added PheromoneMap class and implemented basic behaviour

--- Diffusion.py
+++ Diffusion.py
@@ -9,9 +9,31 @@ def diffuse(mapp):
 	return scipy.signal.convolve(mapp, rate)
 
 
-map = np.zeros((400,400))
+class PheromoneMap(Object):
+	def __init__(self, dimension, resolution = 1):
+		#elements 
+		self.resolution = resolution
 
+		self.phero_map = np.zeros(tuple(dimension * resolution), dtype=np.float)
 
+		self.diffusion_matrix = np.array([[0.0,0.2,0.0],[0.2,0.0,0.2],[0.0,0.2,0.0]])
 
-diffuse(map)
-plt.imshow(map)
+	def tick(self, delta):
+		self.phero_map = scipy.signal.convolve(self.phero_map, self.diffusion_matrix * delta)
+
+	def convert_coordinates(self, position):
+		return np.array(position * self.resolution, dtype=np.int)
+
+	def get_pheromone_concentration(self, position, radius):
+		r = int(radius * self.resolution)
+
+		a, b = self.convert_coordinates(position)
+		#setting up circular mask
+		y,x = np.ogrid[-a:n-a, -b:n-b]
+		mask = x*x + y*y <= r*r
+
+		#disortion of mask
+		# -
+
+		return np.average(self.phero_map[mask])
+
