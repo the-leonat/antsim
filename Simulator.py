44e8e3b0c99246cfb4c163da8a9aecfe22faab0d
leonardpuhl <puhl@campus.tu-berlin.de>
2015-02-12 17:49:40 +0100

    startet pyglet impl

--- Simulator.py
+++ Simulator.py
@@ -8,8 +8,10 @@ import pygame
 import pygame.gfxdraw
 import numpy as np
 import View as view
+import pyglet
 
-class Simulator():
+
+class Simulator(pyglet.window.Window):
     '''
     This class simulates the behavior of worldobjects over time
     '''
@@ -17,59 +19,13 @@ class Simulator():
 
     def __init__(self, mode):
         #always use set_mode function to set mode. dont set string directly
-        self.mode = None
-        self.set_mode(mode)
 
         self.world = World()
 
-        self.screen = None
-        
-        if self.is_mode("live"):
-            self.screen = self.init_screen(800,600)
-            self.screen.fill((255,255,255))
-
-
-
-    def set_mode(self, mode):
-        '''
-        sets the simulation mode to a mode. modes are defined in world.s_modes.
-        '''
-
-        if str(mode) in Simulator.s_modes:
-            self.mode = str(mode)
-        else:
-            print "! mode '" + str(mode) + "' not found. set to '" + Simulator.s_modes[0] + "'"
-            self.mode = Simulator.s_modes[0]
-
-    def is_mode(self, mode):
-        '''
-        checks if mode equals the current mode
-        '''
-        if str(mode) in Simulator.s_modes:
-            if str(mode) == self.mode:
-                return True
-            else:
-                return False
-
-        return False
-
-    def process_pygame_events(self):
-        pygame.event.pump()
-        events = pygame.event.get()
-
-        for event in events:
-            if event.type==pygame.VIDEORESIZE:
-                self.screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
-                self.screen.fill((255,255,255))
-                pygame.display.flip()
-                print "new display size"
-
+        super().__init__(width=512, height=512,visible=True)
 
     def init_screen(self, width, height):
-        pygame.init()
-        size = width, height
-
-        return pygame.display.set_mode(size, pygame.RESIZABLE)
+        return pyglet.window.Window()
 
 
     def simulate_steps(self, n = 1, redraw=True):
@@ -78,8 +34,6 @@ class Simulator():
         optional param n is the number of steps to simulate
         '''
 
-        self.process_pygame_events()
-
         t_start = time.clock()
 
 
@@ -87,19 +41,7 @@ class Simulator():
             #update the world model
             self.world.update()
 
-            if self.is_mode("live") and redraw:
-                self.draw_data()
-                #view.set_phero_map(self.world.phero_map.phero_map)
-                #view.tick()
-                #self.draw_position_over_time()
-
         print "time per frame: ", (time.clock() - t_start) / n
-
-        if self.is_mode("live") and not redraw:
-            self.draw_data()
-            #view.set_phero_map(self.world.phero_map.phero_map)
-            #view.tick()
-            #self.draw_position_over_time()
 
     def s(self, n = 1, redraw=True):
         self.simulate_steps(n, redraw)
@@ -121,71 +63,10 @@ class Simulator():
 
         return position_int
 
-    def draw_position_over_time(self):
 
-        for o in self.world.get_objects():
-            if o.is_type("ant"):
-                x,y = self.convert_coordinates(o.position)
-                self.screen.set_at((x, y), (0,0,0))
-
-
-        # #update the screen
-        pygame.display.flip()
-
-
-
-
-    def draw_data(self):
-        '''git 
-        takes all gameobjects and renders them
-        '''
-        self.screen.fill((255,255,255,0))
-
-        # phero_surface = pygame.Surface((400,400), flags=pygame.SRCALPHA)
-        # phero_surface.fill((50,50,255,0))
-
-        # #render the phero map first then blit ants into it
-        # alpha_data = self.world.phero_map.phero_map.copy()
-        # #convert to rgba alpha format, 0 - 255
-        # alpha_data = alpha_data * 255
-        # alpha_data = alpha_data.astype(dtype=np.uint8)
-
-        # # alpha_map = pygame.surfarray.pixels_alpha(phero_surface)
-        # # alpha_map = alpha_data
-
-        # #suuuper langsam
-        # for y in range(400):
-        #     for x in range(400):
-        #         phero_surface.set_at((x,y), (50,50,255, alpha_data[x,y]))
-
-
-        # self.screen.blit(phero_surface, (0,0))
-
-        #render the ants
-        for o in self.world.get_objects():
-            #print "[" + str(o.type) + "] pos:", o.position
-
-            #render ant
-            if o.is_type("ant"):
-                position_int = self.convert_coordinates(o.position)
-
-                #convert to int
-                direction_int = np.array(np.rint( o.direction * 10), dtype=np.uint32)
-
-                #print "position int: ", position_int
-
-
-                #render position
-                pygame.draw.circle(self.screen, (0,0,0,255), tuple(position_int), o.center_radius, 1)
-
-
-                pygame.draw.line(self.screen, (255,0,0,255), tuple(position_int), tuple(position_int + direction_int), 2)
-
-
-
-        # #update the screen
-        pygame.display.flip()
-
+    @window.event    
+    def draw_data():
+        self.clear()
 
 
 
